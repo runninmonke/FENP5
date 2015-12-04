372ea0738835fdf536318835e339102d8c6f254a
runninmonke <runninmonke@yahoo.com>
2015-12-04 16:09:33 -0600

    refactor: Improve code readability via seperation of concerns

--- js/app.js
+++ js/app.js
@@ -45,100 +45,112 @@ var Place = function(data) {
 	this.status = ko.observable('deselected');
 
 	var selfPlace = this;
-	/* Get info from geocoder and populate place properties with results and add a marker*/
-	self.geocoder.geocode({address: this.address}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			selfPlace.latLng = results[0].geometry.location;
-			selfPlace.placeId = results[0].place_id;
-			selfPlace.marker = new google.maps.Marker({
-				position: selfPlace.latLng,
-				map: self.map,
-				title: selfPlace.name
-			});
-			selfPlace.marker.addListener('click', function(){self.changePlace(selfPlace);});
+	/* Get info from geocoder and call function to populate properties with results and add a marker*/
+	geocoder.geocode({address: this.address}, function(results, status) {selfPlace.applyGeocode(results, status);});
 
-	/* Search to see if Google Places has a place with a matching name, temporarily hold
-	** the returned details, then replace with more detailed details from getDetails() */
-			self.detailService.nearbySearch({location: selfPlace.latLng, radius: '100', name: selfPlace.name}, function(results, status){
-				if (status == google.maps.places.PlacesServiceStatus.OK) {
-					selfPlace.details = results[0];
-					selfPlace.placeId = selfPlace.details.place_id;
-					self.detailService.getDetails({placeId: selfPlace.placeId}, function(results, status){
-						if (status == google.maps.places.PlacesServiceStatus.OK) {
-							selfPlace.details = results;
-						}
-					});
-				}
-			});
-		}
-
-	});
 }
 
+/* Populate properties with Geocoderesults, add a marker and try to get additional details*/
+Place.prototype.applyGeocode = function(results, status) {
+	var selfPlace = this;
+	if (status == google.maps.GeocoderStatus.OK) {
+		this.latLng = results[0].geometry.location;
+		this.placeId = results[0].place_id;
+		this.marker = new google.maps.Marker({
+			position: this.latLng,
+			map: map,
+			title: this.name
+		});
+	this.marker.addListener('click', function(){vm.changePlace(selfPlace);});
+	detailService.nearbySearch({location: this.latLng, radius: '100', name: this.name}, function(results, status){selfPlace.applyDetails(results, status);});
+	}
+}
+
+/* Temporarily assign results from nearbySearch() then assign results from getDetails()*/
+Place.prototype.applyDetails = function(results, status){
+	var selfPlace = this;
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		this.details = results[0];
+		this.placeId = this.details.place_id;
+		detailService.getDetails({placeId: this.placeId}, function(results, status){
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				selfPlace.details = results;
+			}
+		});
+	}
+}
+
+var map;
+var geocoder;
+var infoWindow;
+var detailService;
+var vm;
+
+//https://api.apixu.com/v1/current.json?key=f7fc2a0c018f47c688b200705150412&q=Paris
+var viewModel = function() {
+	vm = this;
+
+	vm.places = [];
+	for (var i in locationData) {
+		vm.places.push(new Place(locationData[i]));
+	}
+
+	vm.activePlaces = ko.computed(function() {
+		var workingArray = [];
+		for (var i in vm.places) {
+			if (vm.places[i].active()) {
+				workingArray.push(vm.places[i]);
+			}
+		}
+		return workingArray;
+	});
+
+	vm.selectedPlace = ko.observable();
+	vm.changePlace = function(place) {
+		if (typeof vm.selectedPlace() == 'object') {
+			vm.selectedPlace().status('deselected');
+			vm.selectedPlace().marker.setAnimation(null);
+			if (place === vm.selectedPlace()) {
+				vm.selectedPlace = ko.observable();
+				infoWindow.close();
+				return;
+			}
+		}
+		vm.selectedPlace(place);
+		vm.selectedPlace().status('selected');
+		vm.selectedPlace().marker.setAnimation(google.maps.Animation.BOUNCE);
+		infoWindow.setContent(vm.selectedPlace().name);
+		infoWindow.open(map, vm.selectedPlace().marker)
+
+	}
+
+	vm.searchTerm = ko.observable("");
+	vm.searchTerm.extend({ rateLimit: { timeout: 400, method: "notifyWhenChangesStop" } });
+	vm.searchPlaces = function() {
+		var workingPlace = '';
+		var workingSearchTerm = vm.searchTerm().toLowerCase();
+		for (var i in vm.places) {
+			workingPlace = vm.places[i].name.toLowerCase();
+			if (workingPlace.indexOf(workingSearchTerm) > -1) {
+				vm.places[i].active(true);
+				vm.places[i].marker.setMap(map);
+			} else {
+				vm.places[i].active(false);
+				vm.places[i].marker.setMap(null);
+			}
+		}
+	}
+	vm.searchTerm.subscribe(vm.searchPlaces);
+};
+
 var initMap = function() {
-	var viewModel = function() {
-		self = this;
-		self.map = new google.maps.Map(document.getElementById('map'), {
-			center: neighborhood.center,
-			zoom: 14
-		});
-		self.geocoder = new google.maps.Geocoder();
-		self.infoWindow = new google.maps.InfoWindow();
-		self.detailService = new google.maps.places.PlacesService(self.map);
-
-		self.places = [];
-
-		for (var i in locationData) {
-			self.places.push(new Place(locationData[i]));
-		}
-
-		self.activePlaces = ko.computed(function() {
-			var workingArray = [];
-			for (var i in self.places) {
-				if (self.places[i].active()) {
-					workingArray.push(self.places[i]);
-				}
-			}
-			return workingArray;
-		});
-
-		self.selectedPlace = ko.observable();
-		self.changePlace = function(place) {
-			if (typeof self.selectedPlace() == 'object') {
-				self.selectedPlace().status('deselected');
-				self.selectedPlace().marker.setAnimation(null);
-				if (place === self.selectedPlace()) {
-					self.selectedPlace = ko.observable();
-					self.infoWindow.close();
-					return;
-				}
-			}
-			self.selectedPlace(place);
-			self.selectedPlace().status('selected');
-			self.selectedPlace().marker.setAnimation(google.maps.Animation.BOUNCE);
-			self.infoWindow.setContent(self.selectedPlace().name);
-			self.infoWindow.open(self.map, self.selectedPlace().marker)
-
-		}
-
-		self.searchTerm = ko.observable("");
-		self.searchTerm.extend({ rateLimit: { timeout: 400, method: "notifyWhenChangesStop" } });
-		self.searchPlaces = function() {
-			var workingPlace = '';
-			var workingSearchTerm = self.searchTerm().toLowerCase();
-			for (var i in self.places) {
-				workingPlace = self.places[i].name.toLowerCase();
-				if (workingPlace.indexOf(workingSearchTerm) > -1) {
-					self.places[i].active(true);
-					self.places[i].marker.setMap(self.map);
-				} else {
-					self.places[i].active(false);
-					self.places[i].marker.setMap(null);
-				}
-			}
-		}
-		self.searchTerm.subscribe(self.searchPlaces);
-	};
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: neighborhood.center,
+		zoom: 14
+	});
+	geocoder = new google.maps.Geocoder();
+	infoWindow = new google.maps.InfoWindow();
+	detailService = new google.maps.places.PlacesService(map);
 
 	ko.applyBindings(new viewModel());
 };
